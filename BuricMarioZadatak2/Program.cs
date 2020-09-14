50e85e0999d5a43f5bb037abc7d92c32d7e3ae5b
MarioBuric <55134593+MarioBuric@users.noreply.github.com>
2020-09-14 23:18:01 +0200

    Add files via upload

---
+++ BuricMarioZadatak2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuricMarioZadatak2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int seconds;
+            int minutes;
+            int sec;
+
+
+            Console.WriteLine("Type seconds");
+
+            seconds = Convert.ToInt32(Console.ReadLine());
+
+            minutes = seconds / 60;
+            sec = seconds % 60;
+            Console.WriteLine("{1} seconds and {0} minutes", minutes, sec);
+            Console.ReadKey();
+
+
+        }
+    }
+}
